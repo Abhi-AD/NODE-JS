b55f7e80d4dcf3d8a898740b5290b4cc3aeb97cc
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-03-10 18:22:13 +0545

    stream in a javascripts

--- Course/6-JSON.js
+++ Course/6-JSON.js
@@ -1,6 +1,11 @@
 let fs = require('fs');
 const http = require('http');
 const url = require('url');
+const events = require('events');
+
+// user define modules
+const replaceHtml = require('../Module/replaceHtml')
+const users = require('../Module/users')
 
 
 const html = fs.readFileSync('../Template/index.html', 'utf-8')
@@ -9,21 +14,6 @@ let productslist = fs.readFileSync('../Template/products-list.html', 'utf-8');
 let productsdetail = fs.readFileSync('../Template/products-details.html', 'utf-8');
 
 
-function replaceHtml(template, product) {
-     let output = template.replace('{{%IMAGE%}}', product.productImage);
-     output = output.replace('{{%NAME%}}', product.name);
-     output = output.replace('{{%MODELNAME%}}', product.modeName);
-     output = output.replace('{{%MODELNUMBER%}}', product.modelNumber);
-     output = output.replace('{{%SIZE%}}', product.size);
-     output = output.replace('{{%CAMERA%}}', product.camera);
-     output = output.replace('{{%PRICE%}}', product.price);
-     output = output.replace('{{%COLOR%}}', product.color);
-     output = output.replace('{{%ID%}}', product.id);
-     output = output.replace('{{%ROM%}}', product.ROM);
-     output = output.replace('{{%desc%}}', product.Description);
-
-     return output;
-}
 
 
 
@@ -41,7 +31,59 @@ function replaceHtml(template, product) {
 
 
 // createing a simple web server routes
-const server = http.createServer((request, response) => {
+// const server = http.createServer((request, response) => {
+//      let { query, pathname: path } = url.parse(request.url, true)
+//      //     console.log(x)
+//      // let path = request.url;
+//      if (path === '/' || path.toLocaleLowerCase() === '/home') {
+//           response.writeHead(200, {
+//                'Content-Type': 'text/html',
+//                'my-header': 'hello  world!'
+//           })
+//           response.end(html.replace('{{%CONTENT%}}', 'You are in Home Page'))
+//      }
+//      else if (path.toLocaleLowerCase() === "/about") {
+//           response.writeHead(200, {
+//                'Content-Type': 'text/html',
+//                'my-header': 'hello  world!'
+//           })
+//           response.end(html.replace('{{%CONTENT%}}', 'You are in About Page'))
+//      }
+//      else if (path.toLocaleLowerCase() === "/contact") {
+//           response.writeHead(200, {
+//                'Content-Type': 'text/html',
+//                'my-header': 'hello  world!'
+//           })
+//           response.end(html.replace('{{%CONTENT%}}', 'You are in Contact Page'))
+//      }
+//      // working the json file
+//      else if (path.toLocaleLowerCase() === '/products') {
+//           if (!query.id) {
+//                let productsHtmlArray = products.map((prod) => {
+//                     return replaceHtml(productslist, prod);
+//                })
+//                let productsResponseHtml = html.replace('{{%CONTENT%}}', productsHtmlArray.join(','));
+//                response.writeHead(200, { 'Content-Type': 'text/html' });
+//                response.end(productsResponseHtml);
+//           } else {
+//                let product_id = products[query.id]
+//                let productDetailResponseHtml = replaceHtml(productsdetail, product_id);
+//                response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml));
+//           }
+//      }
+//      else {
+//           response.writeHead(404, {
+//                'Content-Type': 'text/html',
+//                'my-header': 'hello  world!'
+//           })
+//           response.end(html.replace('{{%CONTENT%}}', 'Error 404: Page not found!'))
+//      }
+// });
+
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
      let { query, pathname: path } = url.parse(request.url, true)
      //     console.log(x)
      // let path = request.url;
@@ -69,7 +111,7 @@ const server = http.createServer((request, response) => {
      // working the json file
      else if (path.toLocaleLowerCase() === '/products') {
           if (!query.id) {
-               let productsHtmlArray = products.map((prod)=>{
+               let productsHtmlArray = products.map((prod) => {
                     return replaceHtml(productslist, prod);
                })
                let productsResponseHtml = html.replace('{{%CONTENT%}}', productsHtmlArray.join(','));
@@ -77,7 +119,7 @@ const server = http.createServer((request, response) => {
                response.end(productsResponseHtml);
           } else {
                let product_id = products[query.id]
-               let productDetailResponseHtml= replaceHtml(productsdetail, product_id);
+               let productDetailResponseHtml = replaceHtml(productsdetail, product_id);
                response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml));
           }
      }
@@ -88,7 +130,16 @@ const server = http.createServer((request, response) => {
           })
           response.end(html.replace('{{%CONTENT%}}', 'Error 404: Page not found!'))
      }
-});
+})
+
+
+
+
+
+
+
+
+
 
 
 
@@ -96,6 +147,20 @@ const server = http.createServer((request, response) => {
 server.listen(8000, '127.0.0.1', () => {
      console.log("The server is running on the server")
 })
+
+
+
+
+
+// Emitter and custom event handling
+let myEmitter = new users();
+myEmitter.on('userCreated', (id, name) => {
+     console.log(`A new user ${name} with Id ${id} is created!`)
+})
+myEmitter.on('userCreated', (id, name) => {
+     console.log(`A new user ${name} with Id ${id} is added to database!`)
+})
+myEmitter.emit("userCreated", 101, 'Abhishek')
 
 
 
---
+++ Course/7-Stream.js
@@ -0,0 +1,45 @@
+let fs = require('fs');
+const http = require('http');
+const url = require('url');
+const events = require('events');
+
+// user define modules
+const replaceHtml = require('../Module/replaceHtml')
+const users = require('../Module/users')
+
+
+const server = http.createServer();
+
+server.listen(8000, '127.0.0.1', () => {
+     console.log("The server is running on the server")
+})
+
+
+// // stream in javascripts (without readable or writeable stream)
+// server.on('request', (req, res) =>{
+//      fs.readFile('../txt/large-file.txt', (err, data) =>{
+//           if(err){
+//                res.end('Something went wrong!');
+//                return;
+//           }
+//           res.end(data)
+//      })
+// })
+
+
+
+
+// stream in javascripts (using the  readable or writeable stream)
+server.on('request', (req, res) =>{
+     let rs = fs.createReadStream('../txt/large-file.txt')
+     rs.on('data', (file) =>{
+          res.write(file)
+          res.end();
+     })
+     rs.on('error', (error) =>{
+          res.end(error.message);
+     })
+})
+
+
+
